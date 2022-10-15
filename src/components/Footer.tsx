be4094c9ae34b30336df47995b5e477ac000d0e1
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-10-15 16:07:47 +0300

    Footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,28 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
-import { BottomNavigationAction, Box } from '@mui/material';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-const Footer = () => {
-    const [value, setValue] = React.useState(0);
-    return (
-        <Box sx={{ width: 500, position: 'sticky' }}>
-            <BottomNavigation
-                showLabels
-                value={value}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                }}
-            >
-                <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-                <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-                <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-            </BottomNavigation>
-        </Box>
-    )
-}
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
 
-export default Footer
+  return (
+    <Box sx={{position: 'fixed', left: 0, right: 0, bottom: 0,
+    boxShadow: 'rgb(114 142 147 / 30%) 0px 5px 15px 0px',backgroundColor: '#1976d2'}}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
