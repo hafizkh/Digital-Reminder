ad32893836f5d3591de589eb46c32d33c0ac37f7
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-10-15 22:22:53 +0300

    Added links in the social media icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,27 +2,29 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function SimpleBottomNavigation() {
-  const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(0);
 
-  return (
-    <Box sx={{position: 'fixed', left: 0, right: 0, bottom: 0,
-    boxShadow: 'rgb(114 142 147 / 30%) 0px 5px 15px 0px',backgroundColor: '#1976d2'}}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
-    </Box>
-  );
+    return (
+        <Box sx={{
+            position: 'fixed', left: 0, right: 50, bottom: 0,
+            boxShadow: 'rgb(114 142 147 / 30%) 0px 5px 15px 0px', backgroundColor: '#1976d2'
+        }}>
+            <BottomNavigation
+                showLabels
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+            >
+                <BottomNavigationAction icon={<FacebookIcon />} href="https://www.facebook.com/khurram.javid.47" target="_blank" rel="noopener" />
+                <BottomNavigationAction icon={<LinkedInIcon />} href="https://www.linkedin.com/in/hafiz-javid-6a0810225/" target="_blank" rel="noopener" />
+                <BottomNavigationAction icon={<GitHubIcon />} href="https://github.com/hafizkh/" target="_blank" rel="noopener" />
+            </BottomNavigation>
+        </Box>
+    );
 }
