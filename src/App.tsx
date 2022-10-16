b2d9831df626a6e9887e1556a79a9af6f7d04e64
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-10-16 03:00:02 +0300

    To do component added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import ToDo from './components/ToDo';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
+      <ToDo />
 
     </div>
   );
---
+++ src/components/ToDo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ToDo = () => {
+  return (
+    <div>
+        <h1>This is to do component</h1>
+    </div>
+  )
+}
+
+
+export default ToDo
