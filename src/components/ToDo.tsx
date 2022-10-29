bb7bd777832a8dbe71889633f8c2dd6aa9d4df21
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-10-29 18:25:10 +0300

    Got the list in console

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,20 +1,35 @@
-import React, { FC } from 'react'
+import React, { FC, useState, ChangeEvent } from 'react'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import '../styles/_todo.scss'
+import { TasksInterface } from '../Category/Interface'
 
 
 const ToDo: FC = () => {
+  const [task, setTask] = useState<string>("")
+  const [todoList, setTodoList] = useState<TasksInterface[]>([])
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value)
+    setTask(e.target.value)
+  }
+
+  const addTask = (): void => {
+    const taskNew = { taskName: task }
+    setTodoList([...todoList, taskNew])
+    console.log(todoList);
+  }
   return (
-      <Stack className='toDoTask' direction="row" spacing={1}>
-          <TextField
-            hiddenLabel
-            label="Your Task"
-            id="outlined-size-small"
-            size="small"
-          />
-        <Button variant='outlined'>Add Task</Button>
+    <Stack className='toDoTask' direction="row" spacing={1}>
+      <TextField
+        hiddenLabel
+        name='yourTask'
+        label="Your New Task"
+        id="outlined-size-small"
+        size="small"
+        onChange={handleChange}
+      />
+      <Button variant='outlined' onClick={addTask}>Add Task</Button>
 
     </Stack>
   )
