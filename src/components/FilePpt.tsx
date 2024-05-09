8b798671662e7bc3d589b510a6fc8dd622b895f1
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2024-05-09 23:40:49 +0300

    wazuh ppt

--- src/components/FilePpt.tsx
+++ src/components/FilePpt.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 
+
 const OfficeViewer: FC = () => {
+    const embedUrl = process.env.PUBLIC_URL + '/WAZUH_presentation.pptx';
 
     return (
         <div style={{ margin: '0 10px' }}>
-            <a href="https://hameenamk-my.sharepoint.com/:p:/r/personal/hafiz21000_student_hamk_fi/_layouts/15/Doc.aspx?sourcedoc=%7B2BDEEC8F-273A-4423-AEE8-C1DE94B5A608%7D&file=WAZUH_presentation.pptx&action=edit&mobileredirect=true&DefaultItemOpen=1&login_hint=hafiz21000%40student.hamk.fi&ct=1715286478705&wdOrigin=OFFICECOM-WEB.MAIN.EDGEWORTH&cid=b06e8a9e-0c87-420d-86ad-20f648b27465&wdPreviousSessionSrc=HarmonyWeb&wdPreviousSession=8979c36f-204f-462e-a93e-34ade67f360b" download="Presentation.ppt" style={{ color: 'inherit', textDecoration: 'none' }}>
+            <a href={embedUrl} download="Wazuh.ppt" style={{ color: 'inherit', textDecoration: 'none' }}>
                 Thesis Presentation
             </a>
         </div>
