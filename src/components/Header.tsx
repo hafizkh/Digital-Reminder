a0dfbeae5bd3e3de8b0bf09a1eb61e6886643b6d
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2024-05-09 23:30:43 +0300

    ppt added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,23 +4,28 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import FilePpt from './FilePpt';
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-          </IconButton>
-          <Typography variant="h6" component="div" >
-            Digital Reminder
-          </Typography>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              {/* Icon can go here if you have one, e.g., MenuIcon */}
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Digital Reminder
+            </Typography>
+          </Box>
+          <FilePpt />
         </Toolbar>
       </AppBar>
     </Box>
